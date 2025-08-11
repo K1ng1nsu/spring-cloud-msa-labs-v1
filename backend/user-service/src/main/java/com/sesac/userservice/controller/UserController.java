588dcaaccc9834869610a1b13ec08a32b1945e22
K1ng1nsu <96197161+K1ng1nsu@users.noreply.github.com>
2025-08-11 14:21:37 +0900

    feat: Add login feature to user-service with JWT token
    - Add dependency - spring security
    - Add feature - Login
    - Change Initial SQL script

--- backend/user-service/src/main/java/com/sesac/userservice/controller/UserController.java
+++ backend/user-service/src/main/java/com/sesac/userservice/controller/UserController.java
@@ -1,14 +1,13 @@
 package com.sesac.userservice.controller;
 
+import com.sesac.userservice.dto.LoginRequest;
+import com.sesac.userservice.dto.LoginResponse;
 import com.sesac.userservice.entity.User;
 import com.sesac.userservice.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,13 +16,24 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/{id}")
-    @Operation(summary = "사용자 조회", description = "ID로 사용자를 조회합니다.")
+    @Operation(summary = "Search User", description = "Search User with ID")
     public ResponseEntity<?> getUserById(@PathVariable Long id) {
         try {
             User byId = userService.findById(id);
             return ResponseEntity.ok(byId);
         } catch (RuntimeException e) {
             return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("/login")
+    @Operation(summary = "login", description = "login with email and password. you will get JWT token")
+    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
+        try {
+            LoginResponse login = userService.login(loginRequest);
+            return ResponseEntity.ok(login);
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
 
--- backend/user-service/src/main/java/com/sesac/userservice/service/UserService.java
+++ backend/user-service/src/main/java/com/sesac/userservice/service/UserService.java
@@ -1,8 +1,12 @@
 package com.sesac.userservice.service;
 
+import com.sesac.userservice.dto.LoginRequest;
+import com.sesac.userservice.dto.LoginResponse;
 import com.sesac.userservice.entity.User;
 import com.sesac.userservice.repository.UserRepository;
+import com.sesac.userservice.security.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,10 +16,25 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final PasswordEncoder passwordEncoder;
+
 
     public User findById(Long id) {
         return userRepository.findById(id).orElseThrow(
                 () -> new RuntimeException("User not found id: " + id));
+    }
+
+    public LoginResponse login(LoginRequest loginRequest) {
+        User byEmail = userRepository.findByEmail(loginRequest.getEmail()).orElseThrow(() -> new RuntimeException("Invalid email or password"));
+
+        if (!passwordEncoder.matches(loginRequest.getPassword(), byEmail.getPassword()))
+            throw new RuntimeException("Invalid email or password");
+
+        String token = jwtTokenProvider.generateToken(byEmail.getEmail(), byEmail.getId());
+
+
+        return new LoginResponse(token, byEmail.getId(), byEmail.getEmail(), byEmail.getName());
     }
 
 
