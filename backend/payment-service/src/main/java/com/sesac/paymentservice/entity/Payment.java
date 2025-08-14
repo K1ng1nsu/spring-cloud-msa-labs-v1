8bfbf9e9ab27c8081f03c7b0566c434209872443
K1ng1nsu <96197161+K1ng1nsu@users.noreply.github.com>
2025-08-14 12:51:32 +0900

    feat: payment-request-event 수신 구현

---
+++ backend/payment-service/src/main/java/com/sesac/paymentservice/entity/Payment.java
@@ -0,0 +1,46 @@
+package com.sesac.paymentservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "payments")
+@Entity
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Column(nullable = false, unique = true)
+    private Long orderId;
+
+    @Column(nullable = false)
+    private BigDecimal amount;
+
+
+    @Column(nullable = false)
+    private PaymentStatus status;
+
+    @Column(nullable = false)
+    private String paymentMethod;
+
+    private String failureReason;
+
+    @CreationTimestamp
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+}
--- backend/payment-service/src/main/java/com/sesac/paymentservice/event/product/in/ProductEventListener.java
+++ backend/payment-service/src/main/java/com/sesac/paymentservice/event/product/in/ProductEventListener.java
@@ -2,10 +2,17 @@ package com.sesac.paymentservice.event.product.in;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class ProductEventListener {
+
+    @RabbitListener(queues = "${order.event.queue.payment-request}")
+    public void handleOrderCreatedEvent(PaymentRequestEvent paymentRequestEvent) {
+        log.info("Received Order Created Event with orderId: {}, amount: {}", paymentRequestEvent.getOrderId(), paymentRequestEvent.getTotalAmount());
+
+    }
 }
